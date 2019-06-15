3384073be871da09392ee604f8715cace3b4886c
ashishmangla0 <ashishmangla1991@gmail.com>
2019-06-15 16:03:17 +0530

    Update variables.dart

--- variables.dart
+++ variables.dart
@@ -1,1 +1,4 @@
 
+//Default value
+//Uninitialized variables have an initial value of null.
+//Even variables with numeric types are initially null, because numbers—like everything else in Dart—are objects.
