ba43a21da1cf1c307381ac018516108247b962cc
ashishmangla0 <ashishmangla1991@gmail.com>
2019-06-15 16:46:35 +0530

    Update variables.dart

--- variables.dart
+++ variables.dart
@@ -1,8 +1,6 @@
 // numbers variables
-
   //int 
   //double
-
 //strings
 //booleans
 //Lists(also know as Arrays)
@@ -10,47 +8,37 @@
 //Runes (for expressing unicode characters in a String)
 //symbols
 
-
-
-
 //Default value
 //Uninitialized variables have an initial value of null.
 //Even variables with numeric types are initially null, because numbers—like everything else in Dart—are objects
-
-
 void main(){
   int abc;  
     print(abc);
   //output will be "null"
-  
    //we can initializing variables with var keyword
-  
-  
   //numbers
     //int 
       int c =  20;
       print (c);
       var b =  10;
       print (b);
-  
+   	  int hexValue = 0xEADEBAEE;
+      print(hexValue);
     //double
+      double exponents = 1.42e5; 
+      print(exponents);
       double e = 3.3;
       print(e);
       var f = 4.4;
       print(f);
-  
   //String
       String name = "yahoo";
       print(name);
       var foo = "this is string";
       print(foo);
-  
   //bools
       bool studing = true;  
       print(studing);
-  
       var notstuding =  false;
       print(notstuding);
-  
-  
 }
